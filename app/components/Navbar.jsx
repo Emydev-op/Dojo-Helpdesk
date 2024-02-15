a4e2a5129136c31df7a7ddab34e57066e989da92
Emydev-op <alphaoseghe@gmail.com>
2024-02-15 06:41:49 +0100

    data

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import logo from "../../public/dojo-logo.png";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
-        <h1>Dojo Helpdesk</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
+      <Image
+        src={logo}
+        alt="Dojo Helpdesk Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
-  )
+  );
 }
