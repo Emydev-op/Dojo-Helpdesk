65dafc66eb3aabf5668c7a5701e107042fc29130
Emydev-op <alphaoseghe@gmail.com>
2024-02-15 13:00:51 +0100

    create ticket form

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -13,9 +13,11 @@ export default function Navbar() {
         quality={100}
         placeholder="blur"
       />
-      <h1>Dojo Helpdesk</h1>
-      <Link href="/">Dashboard</Link>
+      <Link href="/">
+        <h1>Dojo Helpdesk</h1>
+      </Link>
       <Link href="/tickets">Tickets</Link>
+      <Link href={"/tickets/create"}>Create Ticket</Link>
     </nav>
   );
 }
---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import CreateForm from "./CreateForm";
+
+export default async function CreateTicket() {
+  return (
+    <main>
+      <h2 className="text-center">Open a New Ticket</h2>
+      <CreateForm />
+    </main>
+  );
+}
